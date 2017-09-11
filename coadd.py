ebd233853aad4479e5f5ea0009c6fb7fda54b7bd
Dustin Lang <dstndstn@gmail.com>
2017-09-11 11:53:14 -0400

    add comments to the code, fix some typos

--- coadd.py
+++ coadd.py
@@ -26,6 +26,8 @@ H,W = 50,50
 trueflux = 100.
 Nruns = 10000
 
+# The cases we'll consider.  The list of tuples are the per-pixel
+# noise and PSF standard deviation for the two images.
 for name,imageset in [
         ('Case 1: Same noise and PSF', [(1., 2.), (1., 2.)]),
         ('Case 2: Different noise, same PSF', [(1., 2.), (2., 2.)]),
@@ -37,61 +39,61 @@ for name,imageset in [
     print(name)
     print()
 
+    # We're simulating a single isolated point source
     src = tractor.PointSource(tractor.PixPos(W//2, H//2),
                               tractor.Flux(trueflux))
 
+    # Produce tractor image objects with noise model, PSF model, etc
     tims = []
     for noise, psf_size in imageset:
         tim = tractor.Image(np.zeros((H,W), np.float32),
                             inverr=np.ones((H,W), np.float32) * (1./noise),
                             psf=tractor.NCircularGaussianPSF([psf_size], [1.]),
                             photocal=tractor.LinearPhotoCal(1.))
-        # Create noiseless model image.
+        # Create noiseless model image (simulated image)
         tr = tractor.Tractor([tim], [src])
         mod = tr.getModelImage(0)
         tim.data = mod
         tims.append(tim)
 
-    # Run forced photometry on the set of individual images and record the flux and errors.
+    # First we'll run without any noise added to the images, to get estimates of ideal performance.
+
+    # Run the Simultaneous Fitting method
     tr = tractor.Tractor(tims, [src])
     src.brightness.setParams([0.])
+    # Freeze the source position -- only fit for flux
     src.freezeParam('pos')
-    #print('Before fitting: source:', src)
+    # Freeze the image calibration parameters (PSF model, sky background, photometric calibration, etc)
     tr.freezeParam('images')
-    
     phot = tr.optimize_forced_photometry(variance=True)
-    #print('Forced-photometry of individual images result:', phot)
-    #print('Inverse-variance:', phot.IV)
-    #print('Source:', src)
-    print('Forced-phot of individual images simultaneously =', src.getBrightness().getValue(), '+-', 1./np.sqrt(phot.IV[0]))
-
     simult_err = 1./np.sqrt(phot.IV[0])
+    print('Forced-phot of individual images simultaneously =', src.getBrightness().getValue(), '+-', simult_err)
     
-    # Run forced photometry on each individual image and record the flux and errors.
+    # Run the on each individual image
     indiv_errs = []
     for tim in tims:
         tr = tractor.Tractor([tim], [src])
         src.brightness.setParams([0.])
         tr.freezeParam('images')
-        #print('Before fitting: source:', src)
-    
         phot = tr.optimize_forced_photometry(variance=True)
-        #print('Forced-photometry of individual image result:', phot)
-        #print('Inverse-variance:', phot.IV)
-        #print('Source:', src)
-        indiv_errs.append(1./np.sqrt(phot.IV[0]))
-        print('Forced-phot of individual image =', src.getBrightness().getValue(), '+-', 1./np.sqrt(phot.IV[0]))
-        
-    total_indiv_errs = 1./np.sqrt(np.sum(1./np.array(indiv_errs)**2))
+        indiv_err = 1./np.sqrt(phot.IV[0])
+        indiv_errs.append(indiv_err)
+        print('Forced-phot of individual image =', src.getBrightness().getValue(), '+-', indiv_err)
+
+    # Compute the Single-Frame Average error estimate -- these are
+    # Gaussian measurements, so the mean is inverse-variance-weighted,
+    # and the inverse-variance is summed.
+    averaging_err = 1./np.sqrt(np.sum(1./np.array(indiv_errs)**2))
         
     # Compute a coadd (with different coadd weights) and run forced phot on that.
-    ivs = []
+    # Search this grid of alpha values
     coadd_amps = np.linspace(0.0, 1.0, 101)
-    for amp0 in coadd_amps:
+    coadd_errs = np.zeros(len(coadd_amps))
+    for iamp,amp0 in enumerate(coadd_amps):
         assert(len(tims) == 2)
         amp1 = 1. - amp0
         amps = [amp0, amp1]
-        #print('Coadd amplitudes:', amps)
+        # Compute the coadded image, and its PSF model.
         coadd = np.zeros(tims[0].shape, np.float32)
         coadd_var = np.zeros_like(coadd)
         coadd_psf_sigma = []
@@ -107,32 +109,30 @@ for name,imageset in [
             coadd_psf_sigma.extend(tim.psf.sigmas)
             coadd_psf_weight.extend([w * amp for w in tim.psf.weights])
 
+        # This is the tractor Image object for the coadd
         tim = tractor.Image(
             data=coadd, invvar=1./coadd_var,
             psf=tractor.NCircularGaussianPSF(coadd_psf_sigma,coadd_psf_weight),
             photocal=tractor.LinearPhotoCal(1.))
-
-        #print('PSF sigmas:',  tim.psf.sigmas.getParams())
-        #print('PSF weights:', tim.psf.weights.getParams())
-
+        # Run force photometry on the coadd.
         tr = tractor.Tractor([tim], [src])
         src.brightness.setParams([0.])
         tr.freezeParam('images')
-        #print('Before fitting: source:', src)
         phot = tr.optimize_forced_photometry(variance=True)
-        #print('Forced-photometry of coadd result:', phot)
-        #print('Inverse-variance:', phot.IV)
-        #print('Source:', src)
-        print('Forced-phot of coadd with amp:', amp0, '=', src.getBrightness().getValue(), '+-', 1./np.sqrt(phot.IV[0]))
-        flux_iv = phot.IV[0]
-        ivs.append(flux_iv)
+        coadd_err = 1./np.sqrt(phot.IV[0])
+        print('Forced-phot of coadd with amp:', amp0, '=',
+              src.getBrightness().getValue(), '+-', coadd_err)
+        coadd_errs[iamp] = coadd_err
 
-    errors = 1. / np.sqrt(np.array(ivs))
-
-    i = np.argmin(errors)
+    # Find the coadd amplitude (alpha) that yields the smallest error
+    i = np.argmin(coadd_errs)
     bestamp = coadd_amps[i]
     print('Best coadd amplitude:', bestamp)
+    print('Predicted error at best coadd amplitude:', coadd_errs[i])
+    print('vs single-frame averaging:', averaging_err)
+    print('Factor:', coadd_errs[i] / averaging_err)
 
+    # plot styles
     stys = [dict(color='b', linestyle='dashed'),
             dict(color='b', linestyle='dotted'),]
     
@@ -143,23 +143,22 @@ for name,imageset in [
         plt.axhline(err, label=('Image %s: noise %.1f, seeing %.1f' %
                                 (chr(ord('A')+i), noise,seeing)),
                     zorder=18, **sty)
-    plt.plot(coadd_amps, errors, 'g-', label='Method C: Coadd',
+    plt.plot(coadd_amps, coadd_errs, 'g-', label='Method C: Coadd',
              zorder=22, lw=3, alpha=0.5)
-    plt.axhline(total_indiv_errs, color='k', lw=5, alpha=0.3,
-                label='Method B: Single-frame average',
-                zorder=20,
-        )
+    plt.axhline(averaging_err, color='k', lw=5, alpha=0.3,
+                label='Method B: Single-frame average', zorder=20)
     plt.axhline(simult_err, color='r', label='Method A: Simultaneous fitting',
                 zorder=21)
     l = plt.legend(loc='center right')
     l.set_zorder(30)
-    #plt.title(name)
     plt.xlim(0,1)
     ps.savefig()
 
+
+    # Now we run the noise-adding simulations.
+    
     fluxes_simult = []
-    fluxes_indiv = []
-    fluxes_summed = []
+    fluxes_averaged = []
     fluxes_coadd = []
 
     orig_data = [tim.data for tim in tims]
@@ -179,7 +178,7 @@ for name,imageset in [
         phot = tr.optimize_forced_photometry()
         fluxes_simult.append(src.getBrightness().getValue())
 
-        # Fitting individual exposures
+        # Fit individual exposures
         fluxes = np.zeros(len(tims))
         fluxes_iv = np.zeros_like(fluxes)
         for itim,tim in enumerate(tims):
@@ -190,13 +189,10 @@ for name,imageset in [
             f = src.getBrightness().getValue()
             fluxes[itim] = f
             fluxes_iv[itim] = phot.IV[0]
-
             all_fluxes[irun,itim] = f
 
-        fluxes_indiv.extend(list(fluxes))
-        
         # Weighted sum of individual exposures (weighted by inverse-variance)
-        fluxes_summed.append((np.sum(fluxes * fluxes_iv) / np.sum(fluxes_iv)))
+        fluxes_averaged.append((np.sum(fluxes * fluxes_iv) / np.sum(fluxes_iv)))
 
         # Coadd using best amplitude found above
         coadd = np.zeros(tims[0].shape, np.float32)
@@ -226,37 +222,30 @@ for name,imageset in [
         phot = tr.optimize_forced_photometry()
         fluxes_coadd.append(src.getBrightness().getValue())
 
+    # Histogram the simulation results.
     plt.clf()
     ha = dict(histtype='step', range=(80, 120), bins=40, normed=True)
 
-    # plt.hist(fluxes_indiv, color='b',
-    #          label=('Individual exposures: std %.2f, S/N %.2f' % 
-    #                 (np.std(fluxes_indiv), trueflux / np.std(fluxes_indiv))), **ha)
-
-    #for i in range(len(tims)):
     f = all_fluxes[:,0]
     ha2 = ha.copy()
     ha2.update(stys[0])
-    plt.hist(f, label='Image A: std %.2f, S/N %.2f' % (np.std(f), trueflux / np.std(f)),
-             **ha2)
+    plt.hist(f, label='Image A: std %.2f, S/N %.2f' % (np.std(f), trueflux / np.std(f)), **ha2)
     f = all_fluxes[:,1]
     ha2 = ha.copy()
     ha2.update(stys[1])
-    plt.hist(f, label='Image B: std %.2f, S/N %.2f' % (np.std(f), trueflux / np.std(f)),
-             **ha2)
+    plt.hist(f, label='Image B: std %.2f, S/N %.2f' % (np.std(f), trueflux / np.std(f)), **ha2)
     
     plt.hist(fluxes_simult, color='r',
              label=('[A] Simultaneous fitting: std %.2f, S/N %.2f' %
                     (np.std(fluxes_simult), trueflux / np.std(fluxes_simult))), lw=3, **ha)
-    plt.hist(fluxes_summed, color='k',
+    plt.hist(fluxes_averaged, color='k',
              label=('[B] Single-frame average: std %.2f, S/N %.2f' %
-                    (np.std(fluxes_summed), trueflux / np.std(fluxes_summed))), **ha)
+                    (np.std(fluxes_averaged), trueflux / np.std(fluxes_averaged))), **ha)
     plt.hist(fluxes_coadd, color='g',
              label=('[C] Coadded: std %.2f, S/N %.2f' %
                     (np.std(fluxes_coadd), trueflux / np.std(fluxes_coadd))), **ha)
     plt.xlabel('Measured flux')
     plt.ylabel('Frequency')
-    plt.title(name)
     plt.legend(loc='upper right')
     yl,yh = plt.ylim()
     plt.ylim(yl, yh*1.5)
